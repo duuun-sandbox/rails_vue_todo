3ffe0b6a1be773f368ac39386278666377e8725b
Duuun <daimura10@gmail.com>
2020-04-11 23:26:17 +0900

    [add] fast_json_api

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TodosController < ApplicationController
   def index
     todos = Todo.all.order(created_at: :desc)
-    render json: todos
+    render json: TodoSerializer.new(todos).serialized_json
   end
 end
